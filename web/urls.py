22aeb432fbb254bb0fac77db1201c25e76c8c8b9
ahsz110 <ahsz110@gmail.com>
2018-09-10 16:16:00 +0430

    'add_static-files-and-complete_register-page'

--- web/urls.py
+++ web/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^submit/expense/$', views.submit_expense, name='submit_expense'),
     url(r'^submit/income/$', views.submit_income, name='submit_income'),
+    url(r'^accounts/register/$', views.register, name='register'),
+    url(r'^$', views.index, name='index'),
 ]
